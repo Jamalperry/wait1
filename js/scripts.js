4570212917227ebdd1e29cfae73b178ea15bbe5f
Jamalperry <lee.perry400@gmail.com>
2019-10-21 14:51:49 -0400

    add images, text, css, scripts and more to initial build

---
+++ js/scripts.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+	$(".img01").click(function() {
+		$(".verse1").fadeToggle();
+	});
+	$(".img02").click(function() {
+		$(".verse2").fadeToggle();
+	});
+	$(".img021").click(function() {
+		$(".verse21").fadeToggle();
+	});
+	$(".img03").click(function() {
+		$(".verse3").fadeToggle();
+	});
+	$(".img04").click(function() {
+		$(".verse4").fadeToggle();
+	});
+	$(".img05").click(function() {
+		$(".verse5").fadeToggle();
+	});
+   $(".img06").click(function() {
+		$(".verse6").slideToggle();
+	});
+	$(".img07").click(function() {
+		$(".verse7").fadeToggle();
+	});
+	$(".img08").click(function() {
+		$(".verse8").fadeToggle();
+	});
+	$(".img09").click(function() {
+		$(".verse9").fadeToggle();
+	});
+	$(".img10").click(function() {
+		$(".verse10").fadeToggle();
+	});
+	$(".img11").click(function() {
+		$(".verse11").fadeToggle();
+	});
+	$(".img12").click(function() {
+		$(".verse12").fadeToggle();
+	});
+	$(".img13").click(function() {
+		$(".verse13").fadeToggle();
+	});
+	$(".img14").click(function() {
+		$(".verse14").fadeToggle();
+	});
+   $(".img15").click(function() {
+		$(".verse15").fadeToggle();
+	});
+	$(".img16").click(function() {
+		$(".verse16").fadeToggle();
+	});
+	$(".img17").click(function() {
+		$(".verse17").fadeToggle();
+	});
+	$(".img18").click(function() {
+		$(".verse18").fadeToggle();
+	});
+    $(".img19").click(function() {
+		$(".verse19").fadeToggle();
+    });
+	$(".img20").click(function() {
+		$(".verse20").fadeToggle();
+	});
+    $(".img21").click(function() {
+		$(".verse21").fadeToggle();
+	});
+	$(".img22").click(function() {
+		$(".verse22").fadeToggle();
+	});
+	$(".img23").click(function() {
+		$(".verse23").fadeToggle();
+	});
+});
